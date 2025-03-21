d13814eed9fdb941f50de7d3c2489b82295992ba
nojong99 <nojong99@gmail.com>
2025-03-21 19:39:17 +0900

    feat: 250321 11문제

---
+++ nojong99/250319_문자열을_정수로_바꾸기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Solution {
+public static int solution(String s) {
+int answer=1;
+if(s.charAt(0)=='-') {
+System.out.print('-');
+for(int i=1; i<s.length(); i++) {
+System.out.print(s.charAt(i));
+answer=s.charAt(i);
+}
+
+```
+	}else {
+
+		for(int i=0; i<s.length(); i++) {
+			System.out.print(s.charAt(i));
+		}
+
+	}
+
+	return answer;
+}
+
+public static void main(String[] args) {
+	Scanner sn = new Scanner(System.in);
+	String s = sn.nextLine();
+
+	solution(s);
+}
+
+```
+
+}
---
+++ nojong99/250320_나머지가_1이_되는_수_찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(true) {
+        	if(n%answer==1) {
+        		
+        		break;
+        	}
+        	answer++;
+        }
+        return answer;
+    }
+}
